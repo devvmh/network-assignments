45cea13229c5f4ca535474d54eeecd0a743f1555
mingqian.ye@gmail.com <mingqian.ye@gmail.com@ab03fee6-7fc7-c0e5-a40b-44d61490659c>
2011-10-13 09:02:09 +0000

    Everything before socket communication is done. Didn't document and clean up the code yet.

--- trunk/a2/AdhocNetworking/gen/ca/uwaterloo/student/cs/dmhoward/adhocnet/R.java
+++ trunk/a2/AdhocNetworking/gen/ca/uwaterloo/student/cs/dmhoward/adhocnet/R.java
@@ -14,15 +14,19 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int IPs_listView=0x7f05000a;
         public static final int lastscantime=0x7f050002;
         public static final int linearLayout1=0x7f050003;
+        public static final int linearLayout2=0x7f050007;
         public static final int myIPaddr=0x7f050001;
         public static final int myMACaddr=0x7f050000;
         public static final int neighbourMACs=0x7f050006;
         public static final int progressBar1=0x7f050005;
+        public static final int progressBar2=0x7f050009;
+        public static final int sendBLSquery_Button=0x7f050008;
+        public static final int startStopServer_button=0x7f05000b;
         public static final int startscan_button=0x7f050004;
-        public static final int startstop_button=0x7f050007;
-        public static final int status=0x7f050008;
+        public static final int status=0x7f05000c;
     }
     public static final class layout {
         public static final int list_item=0x7f030000;
