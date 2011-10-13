08cceb5fff3d451dbd22dec5c8cfebae46e91c82
mingqian.ye@gmail.com <mingqian.ye@gmail.com@ab03fee6-7fc7-c0e5-a40b-44d61490659c>
2011-10-13 22:17:01 +0000

    restructure the code. Better version of adhocNetworking

---
+++ a2/A2/src/com/core/A2Activity.java
@@ -0,0 +1,32 @@
+package com.core;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class A2Activity extends Activity {
+
+
+	private UI view;
+	private Model model;
+	private Controller controller;
+	
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        this.view = new UI(this);
+        this.model = new Model(this, this.view);
+        this.controller = new Controller(this.model);
+        
+        this.view.setController(controller);
+        
+                
+        this.controller.initialize();
+        
+    }
+    
+
+   
+    
+}
