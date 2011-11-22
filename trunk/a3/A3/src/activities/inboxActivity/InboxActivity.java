390f0982f380d210ead3ef831859f6041cc0b7b4
mingqian.ye@gmail.com <mingqian.ye@gmail.com@ab03fee6-7fc7-c0e5-a40b-44d61490659c>
2011-11-22 08:11:19 +0000

    added a popup window for prompting the user to switch to InboxActivity, This is a static method in Client.java

---
+++ trunk/a3/A3/src/activities/inboxActivity/InboxActivity.java
@@ -0,0 +1,13 @@
+package activities.inboxActivity;
+
+import com.a3.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InboxActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inbox);   
+    }
+}
